8d59984d13673613284f5d9655c373f1eb4ee0f5
zhigang <jsnzg@163.com>
2017-05-24 18:39:16 -0500

    add pm

--- mp2.f90
+++ mp2.f90
@@ -2,7 +2,7 @@ This is just an example.
 get molecule
 get basis functions
 get scf data
-do localization here.
+do PM localization here.
 first half integral transformation
 second half integral transformation
 get mp2 energy
